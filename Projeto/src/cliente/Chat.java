303d76cb2caf92824ff861d05f71eabc2f1452a3
Robson <robsondejesus1996@hotmail.com>
2021-08-14 11:13:08 -0300

    Correção chat

--- Projeto/src/cliente/Chat.java
+++ Projeto/src/cliente/Chat.java
@@ -12,35 +12,38 @@ import java.text.SimpleDateFormat;
 
 public class Chat extends GUI {
 
-    private JLabel jl_title;
-    private JEditorPane messages;
-    private JTextField jt_message;
-    private JButton jb_message;
+    private JLabel titulo_dono;
+    private JEditorPane mensagens;
+    private JTextField enviar_mensagem;
+    private JButton btn_mensagem;
     private JPanel panel;
     private JScrollPane scroll;
 
-    private ArrayList<String> message_list;
+    private ArrayList<String> lista_mensagens;
     private Home home;
     private Socket connection;
     private String connection_info;
 
+    /*
+    Recebendo uma string de informaçoes, com quem eu estou me conectando, e o titulo é de quem é esse chat o dono 
+    */
     public Chat(Home home, Socket connection, String connection_info, String title) {
         super("UDESC - Chat " + title);
         this.home = home;
         this.connection_info = connection_info;
-        message_list = new ArrayList<String>();
+        lista_mensagens = new ArrayList<String>();
         this.connection = connection;
-        this.jl_title.setText(connection_info.split(":")[0]);
-        this.jl_title.setHorizontalAlignment(SwingConstants.CENTER);
+        this.titulo_dono.setText(connection_info.split(":")[0]);
+        this.titulo_dono.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     @Override
     protected void inicializarComponentes() {
-        jl_title = new JLabel();
-        messages = new JEditorPane();
-        scroll = new JScrollPane(messages);
-        jt_message = new JTextField();
-        jb_message = new JButton("Enviar");
+        titulo_dono = new JLabel();
+        mensagens = new JEditorPane();
+        scroll = new JScrollPane(mensagens);
+        enviar_mensagem = new JTextField();
+        btn_mensagem = new JButton("Enviar");
         panel = new JPanel(new BorderLayout());
     }
 
@@ -48,26 +51,32 @@ public class Chat extends GUI {
     protected void configurarComponentes() {
         this.setMinimumSize(new Dimension(480, 720));
         this.setLayout(new BorderLayout());
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        messages.setContentType("text/html");
-        messages.setEditable(false);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// pegar apenas o chat, assim não fecha toda a aplicação 
+        mensagens.setContentType("text/html");//configuração para a mensagem ficar mais bonita no formato html 
+        mensagens.setEditable(false);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        jb_message.setSize(100, 40);
+        btn_mensagem.setSize(100, 40);
     }
 
     @Override
     protected void inserirComponenetes() {
-        this.add(jl_title, BorderLayout.NORTH);
+        this.add(titulo_dono, BorderLayout.NORTH);
         this.add(scroll, BorderLayout.CENTER);
         this.add(panel, BorderLayout.SOUTH);
-        panel.add(jt_message, BorderLayout.CENTER);
-        panel.add(jb_message, BorderLayout.EAST);
+        panel.add(enviar_mensagem, BorderLayout.CENTER);
+        panel.add(btn_mensagem, BorderLayout.EAST);
     }
 
+    
+    /*
+    Ações que vão ser executadas na tela
+    quando enviar uma mensagem: ketPressed enviar mensagem 
+    quando precionar o enter: enviar mensagem
+    */
     @Override
     protected void inserirAcoes() {
-        jt_message.addKeyListener(new KeyListener() {
+        enviar_mensagem.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
             }
@@ -84,7 +93,7 @@ public class Chat extends GUI {
             }
         });
         
-        jb_message.addActionListener(event -> send());
+        btn_mensagem.addActionListener(event -> send());
         
         this.addWindowListener(new WindowListener() {
             @Override
@@ -122,15 +131,18 @@ public class Chat extends GUI {
         });
     }
 
+    /*
+    Para quando receber a mensagem na área do editor ser atualizado setando nada no campo "" quando enviar e receber 
+    */
     public void append_message(String received) {
-        message_list.add(received);
+        lista_mensagens.add(received);
         String message = "";
-
-        for (String str : message_list) {
+        //para cada string de str dentro de message, colocar message += str
+        for (String str : lista_mensagens) {
             message += str;
         }
         
-        messages.setText(message);
+        mensagens.setText(message);
     }
 
     @Override
@@ -139,17 +151,22 @@ public class Chat extends GUI {
         this.setVisible(true);
     }
 
+    
+    /*
+    Quando for enviar uma mensagem para alguem 
+    Com o formato de horas, minutos e segundos. 
+    */
     private void send() {
-        DateFormat df = new SimpleDateFormat("hh:mm:ss");
-        message_list.add("<b>[" + df.format(new Date()) + "] Eu: </b><i>" + jt_message.getText() + "</i><br>");
-        Utils.sendMessage(connection, "MESSAGE;<b>[" + df.format(new Date()) + "] " + home.getConnection_info().split(":")[0] + ": </b><i>" + jt_message.getText() + "</i><br>");
+        DateFormat formato = new SimpleDateFormat("hh:mm:ss");
+        lista_mensagens.add("<b>[" + formato.format(new Date()) + "] Eu: </b><i>" + enviar_mensagem.getText() + "</i><br>");
+        Utils.sendMessage(connection, "MESSAGE;<b>[" + formato.format(new Date()) + "] " + home.getConnection_info().split(":")[0] + ": </b><i>" + enviar_mensagem.getText() + "</i><br>");
         String message = "";
         
-        for (String str : message_list) {
+        for (String str : lista_mensagens) {
             message += str;
         }
         
-        messages.setText(message);
-        jt_message.setText("");
+        mensagens.setText(message);
+        enviar_mensagem.setText("");
     }
 }
